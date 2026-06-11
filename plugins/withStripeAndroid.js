2edd9d11cda7ed77096b40ff347a9aa852431e0c
RadowanAhmed <radowanahmed120@gmail.com>
2026-06-11 14:33:38 +0300

    Add Stripe Android dependency

--- plugins/withStripeAndroid.js
+++ plugins/withStripeAndroid.js
@@ -1,17 +1,13 @@
-const { withBuildGradle } = require('expo/config-plugins');
+const { withProjectBuildGradle } = require('expo/config-plugins');
 
 module.exports = function withStripeAndroid(config) {
-    return withBuildGradle(config, (config) => {
+    return withProjectBuildGradle(config, (config) => {
         if (config.modResults.language === 'groovy') {
-            const dependenciesBlock = `
-dependencies {
-    implementation 'com.stripe:stripe-android:20.47.0'
-}
-      `;
+            const dependency = "    implementation 'com.stripe:stripe-android:20.47.0'";
             if (!config.modResults.contents.includes('com.stripe:stripe-android')) {
                 config.modResults.contents = config.modResults.contents.replace(
                     /dependencies\s*{/,
-                    `dependencies {\n    ${dependenciesBlock}`
+                    `dependencies {\n${dependency}`
                 );
             }
         }
